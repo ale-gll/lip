59bbc946a8f7e036998ab7a7d0d4f42b592b357c
Docker <docker@example.com>
2024-11-23 21:06:11 +0000

    Esercizi context free grammar

--- contextfree/lib/exercises.ml
+++ contextfree/lib/exercises.ml
@@ -16,15 +16,50 @@ let todo : grammar =
 
 
 (* #### Exercise 1, easy (zero_n_one_n) *)
-let zero_n_one_n : grammar = todo
-
+let zero_n_one_n : grammar = 
+  {
+    symbols = [ S ];
+    terminals = [ '0'; '1' ];
+    productions =
+      [
+        S --> "0S1"; 
+        S --> "";   
+      ];
+    start = S;
+  }
 
 (* #### Exercise 2, easy (palindromes) *)
-let palindromes : grammar = todo
+let palindromes : grammar = 
+  {
+    symbols = [ S ];
+    terminals = [ '0'; '1' ];
+    productions =
+      [
+        S --> "0S0"; (*0*)
+        S --> "1S1";
+        S --> "0";
+        S --> "1";
+        S --> "";    (*4*)
+      ];
+    start = S;
+  }
 
 
 (* #### Exercise 3, medium (balanced_parentheses) *)
-let balanced_parentheses : grammar = todo
+let balanced_parentheses : grammar = 
+  {
+    symbols = [ S ];
+    terminals = [ '(';')'; '['; ']';'{'; '}' ];
+    productions =
+      [
+        S --> "(S)";
+        S --> "[S]";
+        S --> "{S}";
+        S --> "SS";
+        S --> ""; 
+      ];
+    start = S;
+  }
 
 
 (* #### Exercise 4, hard (same_amount)
@@ -32,4 +67,16 @@ let balanced_parentheses : grammar = todo
    Hint: model the language of words where the number of 0's is
    one greater than the number of 1's and viceversa, then combine them.
 *)
-let same_amount : grammar = todo
+let same_amount : grammar = 
+  {
+    symbols = [ S ];
+    terminals = [ '0'; '1' ];
+    productions =
+      [
+        S --> "0S1";
+        S --> "1S0";
+        S --> "SS";
+        S --> ""; 
+      ];
+    start = S;
+  }
