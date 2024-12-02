5d7fa3f74bb1d20f23653fb929b911616119b967
Docker <docker@example.com>
2024-12-02 14:16:05 +0000

    Toylexer done, Toyparser started

--- toyparser/test/test_toyparser.ml
+++ toyparser/test/test_toyparser.ml
@@ -3,3 +3,10 @@ open Toyparser.Main
 let%test "test_eval_1" = parse "1 + 2 + 3 + (1 + 2)" |> eval = Ok 9
 
 (* YOUR TESTS HERE *)
+let%test "test_eval_2" = parse "(1+2)+4" |> eval = Ok 7
+
+let%test "test_eval_3_fails" = 
+  parse "(1+2)+3+(4+6)" |> eval = Ok 16
+
+let%test "test_parser" = 
+  parse "5+4+1" = Add(Add (Const 5, Const 4), Const 1)
