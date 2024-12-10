94a97992b13f57b186b88fca4cf4dd7d86217373
Docker <docker@example.com>
2024-12-10 14:09:59 +0000

    boolexpr done (up to task 5)

--- expr/boolexpr/boolexprLib/main.ml
+++ expr/boolexpr/boolexprLib/main.ml
@@ -17,16 +17,24 @@ exception NoRuleApplies
 let rec trace1 = function
     If(True,e1,_) -> e1
   | If(False,_,e2) -> e2
-  | If(_,_,_) -> failwith "TODO"
+  | If(e,e1,e2) -> 
+    let e' = trace1 e in
+    If(e',e1,e2) 
   | _ -> raise NoRuleApplies
 
-let rec trace e = try
+let rec trace e = 
+  try
     let e' = trace1 e
     in e::(trace e')
   with NoRuleApplies -> [e]
 
 
+(* eval implements big step semantics*)
 let rec eval = function
     True -> true
   | False -> false
-  | If(_,_,_) -> failwith "TODO"
+  | If(e0,e1,e2) ->
+    ( match eval e0 with
+    | true -> eval e1
+    | false -> eval e2
+    )
--- expr/boolexpr/test/boolexpr.ml
+++ expr/boolexpr/test/boolexpr.ml
@@ -1,12 +1,53 @@
 open BoolexprLib.Main
+open BoolexprLib.Ast
 
 let test_eval expr exp_result =
   (expr |> parse |> eval) = exp_result
 
 (* ### Unit tests for task 4 *)
 
-let%test "test_eval_1" = failwith "TODO"
+let%test "test_eval_1" = test_eval "false" false
+
+let%test "test_eval_2" = test_eval "true" true
+
+let%test "test_eval_3" = test_eval "if true then false else true" false
+
+let%test "test_eval_4" = test_eval "if false then false else true" true
+
+let%test "test_eval_5" = test_eval "if true then (if true then false else true) else (if true then true else false)" false
+
+let%test "test_eval_6" = test_eval "if (if false then false else false) then (if false then true else false) else (if true then false else true)" false
+
+let%test "test_eval_7" = test_eval "if (if (if false then false else false) then (if false then true else false) else (if true then false else true)) then (if false then true else false) else (if true then false else true)" false
+
 
 (* ### Unit tests for task 5 *)
 
-let%test "test_trace1_1" = failwith "TODO"
+(* 1. trace1 makes progress on any If expression (test at most three); *)
+
+let%test "test_trace1_1" = trace1 (If(True, If(False, True, False), True)) = If(False, True, False)
+
+let%test "test_trace1_2" = trace1 (If(False, True, If(True, True, False))) = If(True, True, False)
+
+let%test "test_trace1_3" = trace1 (If(If(True,False,True),True, False)) = If(False, True, False)
+
+(* If trace1 can't make progress on an expression, then it is a value 
+   (Tip: use the is_value predicate from lib/ast.ml); *)
+
+let is_trace1_applicable expr = not(is_value expr)
+
+let%test "test_trace1_cant_progress" = is_trace1_applicable True = false
+
+let%test "trace1_reduces_in_10_steps" =  
+  let expr = 
+    parse "if (if false then false else false) then (if false then true else false) else (if true then false else true)" 
+  in
+  
+  (* Count the number of iterations *)
+  let rec count_trace1_steps e steps =
+    match e with
+    | If(_,_,_) -> count_trace1_steps (trace1 e) (steps+1)
+    | _ -> steps
+  in
+
+  (count_trace1_steps expr 0) <= 10
